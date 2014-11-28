424bf824a817ea7200bd63d76db76cff125f8d07
Charlinho <charlinhocjc@gmail.com>
2014-11-28 00:32:05 -0200

    Primeiro Commit

---
+++ src/main/java/br/com/granbery/tigershoes/model/Cliente.java
@@ -0,0 +1,49 @@
+package br.com.granbery.tigershoes.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Cliente {
+	
+	@Id @GeneratedValue
+	private Long id;
+	
+	private String nome;
+	private String cpf;
+	private String email;
+	private String telefone;
+	private String senha;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+}
