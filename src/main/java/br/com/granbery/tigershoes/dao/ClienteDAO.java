2e57687634be47be0c0b03e1a952254b2b2f8bfd
Luiz <lfandrade.si@gmail.com>
2014-12-04 08:25:24 -0200

    Criação de alguns DAOs

--- src/main/java/br/com/granbery/tigershoes/dao/ClienteDAO.java
+++ src/main/java/br/com/granbery/tigershoes/dao/ClienteDAO.java
@@ -1,7 +1,8 @@
 package br.com.granbery.tigershoes.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
-import javax.persistence.EntityTransaction;
 
 import br.com.granbery.tigershoes.model.Cliente;
 import br.com.granbery.tigershoes.util.JPAUtil;
@@ -14,19 +15,62 @@ public class ClienteDAO {
 	public static ClienteDAO getInstace() {
 		return instance;
 	}
+	private EntityManager em = JPAUtil.getEntityManager();
 	
-	public boolean persistCliente(Object object) {
-		
-		EntityManager em = JPAUtil.getEntityManager();
-		EntityTransaction tx = em.getTransaction();
-		
-		Cliente cliente = (Cliente) object;
-		
-		tx.begin();
-		em.persist(cliente);
-		tx.commit();
-		
-		em.close();
+	public boolean persistCliente(Cliente cliente) {
+		try{
+			em.getTransaction().begin();
+			em.persist(cliente);
+			em.getTransaction().commit();
+		}catch(Exception ex){
+			ex.printStackTrace();
+			em.getTransaction().rollback();
+		}
+			em.close();
 		return true;		
 	}
+	
+	public Cliente getById(final Long id) {
+        return em.find(Cliente.class, id);
+    }
+	
+	@SuppressWarnings("unchecked")
+	public List<Cliente> listClient(Object object){
+		List<Cliente> clientes;
+		clientes = em.createQuery("FROM" +Cliente.class.getName()).getResultList(); 
+		return clientes;
+	}
+	
+	//Faz update do cliente
+	public void merge(Cliente cliente) {
+        try {
+            em.getTransaction().begin();
+            em.merge(cliente);
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            em.getTransaction().rollback();
+        }
+    }
+	
+	public void remove(Cliente cliente) {
+        try {
+            em.getTransaction().begin();
+            cliente = em.find(Cliente.class, cliente.getId());
+            em.remove(cliente);
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            em.getTransaction().rollback();
+        }
+    }
+	public void removeById(final Long id) {
+        try {
+            Cliente cliente = getById(id);
+            remove(cliente);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+	
 }
